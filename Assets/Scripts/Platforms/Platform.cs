9549e00c127c566c1cbe3e200ff2b6d4e9124677
Light3039 <VerodiciaTheNatureGoddess@gmail.com>
2021-04-22 13:15:22 +0430

    Update Platform.cs
    - Minor tidying

--- Assets/Scripts/Platforms/Platform.cs
+++ Assets/Scripts/Platforms/Platform.cs
@@ -1,29 +1,30 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 public class Platform : MonoBehaviour
-/* 
- * TODO:
- *  Go through
+/*
+ * TODO: Go through platforms
  */
 {
+    // Controller
     [SerializeField] private PlatformController controller;
 
+    // Waypoints
     [SerializeField] private List<Vector2> localWayPoints = new List<Vector2>();
 
     private List<Vector2> globalWayPoints = new List<Vector2>();
     private int toWayPoint = 0;
 
-    private Vector2 tweenPosition;
-
+    // Properties
     [SerializeField] private float speed = 1f;
     [SerializeField] private bool isCyclic = false;
+    [SerializeField] private LeanTweenType easeType = LeanTweenType.notUsed;
+
+    // Current state
+    private Vector2 tweenPosition;
 
     private bool isCyclingBack = false;
     private bool isHold = false;
-
-    [SerializeField] private LeanTweenType easeType = LeanTweenType.notUsed;
 
     #region UnityEvents
 
@@ -48,17 +49,17 @@ public class Platform : MonoBehaviour
         {
             if(isCyclic)
             {
-                if (!isCyclingBack)
-                {
-                    toWayPoint++;
-                    if (toWayPoint == globalWayPoints.Count - 1)
-                        isCyclingBack = true;
-                }
-                else
+                if (isCyclingBack)
                 {
                     toWayPoint--;
                     if (toWayPoint == 0)
                         isCyclingBack = false;
+                }
+                else
+                {
+                    toWayPoint++;
+                    if (toWayPoint == globalWayPoints.Count - 1)
+                        isCyclingBack = true;
                 }
             }
             else
@@ -73,11 +74,6 @@ public class Platform : MonoBehaviour
         }
     }
 
-    public void SetHold(bool hold, bool toggle)
-    {
-        isHold = toggle ? !isHold : hold;
-    }
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -85,7 +81,6 @@ public class Platform : MonoBehaviour
         if (Application.isPlaying)
             for (int i = 0; i < globalWayPoints.Count; i++)
             {
-
                 {
                     Gizmos.DrawLine(globalWayPoints[i] + (Vector2.up * .5f), globalWayPoints[i] - (Vector2.up * .5f));
                     Gizmos.DrawLine(globalWayPoints[i] + (Vector2.right * .5f), globalWayPoints[i] - (Vector2.right * .5f));
@@ -102,5 +97,10 @@ public class Platform : MonoBehaviour
     }
 
     #endregion // __UNITY_EVENTS__
+
+    public void SetHold(bool hold, bool toggle = false)
+    {
+        isHold = toggle ? !isHold : hold;
+    }
 
 }
